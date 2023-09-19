17124da63a2908e9a0a9d362e5a32f00c37cde4a
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-09-19 12:33:15 +0900

    chore: prettier, eslintrc, husky pre-commit, pre-push 설정

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  endOfLine: 'lf',
+  trailingComma: 'es5',
+  bracketSpacing: false,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  proseWrap: 'always',
+  arrowParens: 'avoid',
+};
